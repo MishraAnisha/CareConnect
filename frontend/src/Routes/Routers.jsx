2e5815d8a0ac1534f2b70bcc65be1d05ca95a3b3
Ankita Satapathy <145856599+AnkitaSatapathy@users.noreply.github.com>
2024-12-14 18:37:30 +0530

    Update Routers.jsx

--- frontend/src/Routes/Routers.jsx
+++ frontend/src/Routes/Routers.jsx
@@ -6,11 +6,11 @@ import Signup from '../Pages/Signup';
 import Login from '../Pages/Login';
 import Doctors from '../Pages/Doctors/Doctors';
 import Doctorsdetails from '../Pages/Doctors/Doctorsdetails';
-//import MyAccount from "../Dashboard/user-account/MyAccount";
-//import Dashboard from "../Dashboard/doctor-account/Dashboard"
+import MyAccount from "../Dashboard/user-account/MyAccount";
+import Dashboard from "../Dashboard/doctor-account/Dashboard";
 import CheckoutSuccess from '../Pages/CheckoutSuccess';
 import { Routes, Route} from "react-router-dom";
-//import ProtectedRoute from "./ProtectedRoute";
+import ProtectedRoute from "./ProtectedRoute";
 
 
 const Routers = () => {
@@ -26,23 +26,8 @@ const Routers = () => {
         <Route path="/contact" element= {<Contact />}/>
         <Route path="/services" element= {<Services />}/>
         <Route path="/checkout-success" element= {<CheckoutSuccess />}/>
-        {/*<Route
-           path="/users/profile/me"
-           element={
-            <ProtectedRoute allowedRoles={["patient"]}>
-            <MyAccount/>
-            </ProtectedRoute>
-           }
-          />
-        <Route
-           path="/doctors/profile/me"
-           element={
-            <ProtectedRoute allowedRoles={["doctor"]}>
-            <Dashboard/>
-            </ProtectedRoute>
-           }
-          />*/}
-
+        <Route path="/users/profile/me" element={<ProtectedRoute allowedRoles={["patient"]}><MyAccount/>  </ProtectedRoute>}/>
+        <Route path="/doctors/profile/me" element={<ProtectedRoute allowedRoles={["doctor"]}><Dashboard/>  </ProtectedRoute>}/> 
     </Routes>
   )
 }
