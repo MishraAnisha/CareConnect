5cda3c21c6eb5e7185d1c87facef9a36f5ce1d3a
Brs392 <itsbarshaaa@gmail.com>
2024-12-14 11:38:08 +0530

    Add files via upload

---
+++ frontend/src/Routes/Routers.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import Home from '../Pages/Home';
+import Services from '../Pages/Services';
+import Contact from '../Pages/Contact';
+import Signup from '../Pages/Signup';
+import Login from '../Pages/Login';
+import Doctors from '../Pages/Doctors/Doctors';
+import Doctorsdetails from '../Pages/Doctors/Doctorsdetails';
+//import MyAccount from "../Dashboard/user-account/MyAccount";
+//import Dashboard from "../Dashboard/doctor-account/Dashboard"
+import CheckoutSuccess from '../Pages/CheckoutSuccess';
+import { Routes, Route} from "react-router-dom";
+//import ProtectedRoute from "./ProtectedRoute";
+
+
+const Routers = () => {
+  return (
+    <Routes>
+        <Route path="/" element= {<Home />}/>
+        <Route path="/home" element= {<Home />}/>
+        <Route path="/Doctors" element= {<Doctors />}/>
+        <Route path="/Doctors/:id" element= {<Doctorsdetails />}/>
+        <Route path="/Services" element= {<Services />}/>
+        <Route path="/login" element= {<Login />}/>
+        <Route path="/register" element= {<Signup />}/>
+        <Route path="/contact" element= {<Contact />}/>
+        <Route path="/services" element= {<Services />}/>
+        <Route path="/checkout-success" element= {<CheckoutSuccess />}/>
+        {/*<Route
+           path="/users/profile/me"
+           element={
+            <ProtectedRoute allowedRoles={["patient"]}>
+            <MyAccount/>
+            </ProtectedRoute>
+           }
+          />
+        <Route
+           path="/doctors/profile/me"
+           element={
+            <ProtectedRoute allowedRoles={["doctor"]}>
+            <Dashboard/>
+            </ProtectedRoute>
+           }
+          />*/}
+
+    </Routes>
+  )
+}
+
+export default Routers
---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import logo from '../../assets/images/logo.png';
+
+const quicklinks01 = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/",
+    display: "About Us",
+  },
+  {
+    path: "/services",
+    display: "Services",
+  },
+  {
+    path: "/",
+    display: "Blog",
+  },
+];
+
+const quicklinks02 = [
+  {
+    path: "/find-a-doctor",
+    display: "Find a Doctor",
+  },
+  {
+    path: "/",
+    display: "Request an Appointment",
+  },
+  {
+    path: "/",
+    display: "Find a Location",
+  },
+  {
+    path: "/",
+    display: "Got an Opinion",
+  },
+];
+
+const quicklinks03 = [
+  {
+    path: "/",
+    display: "Donate",
+  },
+  {
+    path: "/contact",
+    display: "Contact Us",
+  },
+];
+
+const Footer = () => {
+  const year = new Date().getFullYear();
+  return (
+    <footer className="pb-16 pt-10 bg-[#0F0C1D] text-white">
+      <div className="container ">
+        <div className="flex justify-between flex-col md:flex-row flex-wrap gap-[30px]">
+          <div>
+            <img src={logo} alt="Logo" />
+            <p className="text-[16px] leading-7 font-[400] text-whitesmoke mt-4">
+              Copyright &copy; {year} developed by the commandos. All rights reserved.
+            </p>
+          </div>
+
+          <div>
+            <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-white'>Quick links</h2>
+            <ul>
+              {quicklinks01.map((item,index) => <li key={index} className='mb-4'>
+                <Link to={item.path} className="text-[16px] leading-7 font-[400] text-textColor   ">{item.display}
+              </Link></li>)}
+            </ul>
+          </div>
+
+          <div>
+            <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-white'>I want to:</h2>
+            <ul>
+              {quicklinks02.map((item,index) => <li key={index} className='mb-4'>
+                <Link to={item.path} className="text-[16px] leading-7 font-[400] text-textColor   ">{item.display}
+              </Link></li>)}
+            </ul>
+          </div>
+
+          <div>
+            <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-white'>Support:</h2>
+            <ul>
+              {quicklinks03.map((item,index) => <li key={index} className='mb-4'>
+                <Link to={item.path} className="text-[16px] leading-7 font-[400] text-textColor   ">{item.display}
+              </Link></li>)}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
