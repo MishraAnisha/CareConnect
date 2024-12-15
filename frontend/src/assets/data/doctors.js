cc3ff6987d0d1e0e317b4c1cd39911258482edea
Brs392 <itsbarshaaa@gmail.com>
2024-12-15 19:56:49 +0530

    Add files via upload

---
+++ frontend/src/assets/data/doctors.js
@@ -0,0 +1,36 @@
+import doctorImg01 from "../images/doc1.png";
+import doctorImg02 from "../images/doc4.png";
+import doctorImg03 from "../images/doc3.png";
+
+export const doctors = [
+  {
+    id: "1",
+    name: "Dr.Maya Patel",
+    specialty: "Cardiologist",
+    avgRating: 4.8,
+    totalRating: 272,
+    photo: doctorImg01,
+    totalPatients: 1500,
+    hospital: "KIIMS,Bhubaneswar",
+  },
+  {
+    id: "2",
+    name: "Dr. Karan Singh",
+    specialty: "Pediatrician",
+    avgRating: 4.8,
+    totalRating: 272,
+    photo: doctorImg02,
+    totalPatients: 1500,
+    hospital: "AIIMS,Bhubaneswar",
+  },
+  {
+    id: "3",
+    name: "Dr. Neha Kapoor",
+    specialty: "Dermatologist",
+    avgRating: 4.8,
+    totalRating: 272,
+    photo: doctorImg03,
+    totalPatients: 1500,
+    hospital: "Sum Ultimate,Bhubaneswar",
+  },
+];
