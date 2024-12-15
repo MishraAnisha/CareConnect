9d57e030cc288b89ee6fcaf6871907cf540ad6e5
Ankita Satapathy <145856599+AnkitaSatapathy@users.noreply.github.com>
2024-12-15 12:07:37 +0530

    Add files via upload

---
+++ frontend/src/components/Error/Error.jsx
@@ -0,0 +1,12 @@
+
+import HashLoader from "react-spinners/HashLoader";
+
+
+const Error=({errMessage})=>{
+    return(
+    <div className="flex items-center justify-center w-full h-full">
+        <h3 className="black text-headingColor text-[20px] leading-[30px] font-semibold">{errMessage}</h3>
+        </div>
+    );
+};
+export default Loading;
