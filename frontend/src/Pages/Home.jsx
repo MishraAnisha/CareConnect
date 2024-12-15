64e9e5e98a768950b0021bea45c10d794c6dfb2e
Brs392 <itsbarshaaa@gmail.com>
2024-12-15 19:57:49 +0530

    Update Home.jsx

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import icon01 from '../assets/images/icon01.png';
 import {Link} from 'react-router-dom';
-import {BsArrowRight ,} from 'react-icons/bs'
+import {BsArrowRight } from 'react-icons/bs'
 import icon02 from '../assets/images/icon02.png'
 import icon03 from '../assets/images/icon03.png'
 import About from '../components/About/about';
@@ -15,50 +15,52 @@ import DoctorList from '../components/Doctors/DoctorList';
 const Home = () => {
   return ( 
   <>
+
+
   {/*==========hero section=====*/}
   
   <section className="hero__section pt-[60px] 2xl:h-[800px]">
     <div className="container">
-      <div className='flex flex-col lg: flex-row gap-[90px] items-center justify-between'>
+      <div className='flex flex-col lg: flex-row gap-[90px] items-center justify-center'>
         {/*=========hero content=========*/}
         <div>
           <div className="lg:w-[570px]">
-          <h1 className='text-[20px] leading-[46px] text-[#F7F6FB] font-[700] md:text-[60px] 
-             md:leading-[70px]'>
-             Have a healthy, diseased-free life
-             </h1>
-             <p className="text__para text-[#C0BFBF]">
-             Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-             Vero ex optio aliquam voluptate earum corporis, aliquid, expedita adipisci provident 
-             repellat culpa nulla? Ea odit blanditiis iusto et, dolore dolor 
-              non!
+          <h1 className='text-[20px] leading-[46px] text-[#F9F8FD] font-[700] md:text-[42px] md:leading-[60px] mt-[20px]'>
+            <span className='block'>Prevent, Protect, and Thrive</span>
+            <span className='block text-center mt-2  md:mt-4'>with a Healthy Life</span>
+          </h1>
+
+             <p className="text__para text-[#FFF9F1] text-center">
+             Connecting you to quality healthcare.Trusted professionals delivering personalized care for your wellness journey.
               </p>
-              <button className="btn">Request an Appointment</button>
+              <div className="flex justify-center">
+                <Link to ='/login'><button className="btn hover:bg-[#00004d]">Request an Appointment</button></Link> 
+              </div>
           </div>
           {/*=======hero counter=========*/}
-          <div className='mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row     lg:items-center gap-5 lg:gap-[30px]'>
+          <div className='mt-[30px] lg:mt-[70px] flex flex-col justify-center lg:flex-row lg:items-center gap-5 lg:gap-[30px]'>
             <div>
-              <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-white">
+              <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-[#8AAEFE]">
                 30+
               </h2>
-              <span className='w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]'></span>
-              <p className='text__para text-blue'> Years of Experience</p>
+              <span className='w-[100px] h-2  rounded-full block mt-[-14px]'></span>
+              <p className='text__para text-white'> Years of Experience</p>
             </div>
 
             <div>
-              <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-white">
+              <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-[#8AAEFE]">
                 15+
               </h2>
-              <span className='w-[100px] h-2 bg-purpleColor rounded-full block mt-[-14px]'></span>
-              <p className='text__para text-blue'>Clinic location</p>
+              <span className='w-[100px] h-2  rounded-full block mt-[-14px]'></span>
+              <p className='text__para text-white'>Clinic Location</p>
             </div>
 
             <div>
-              <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-white">
+              <h2 className="text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-[#8AAEFE]">
                 100%
               </h2>
-              <span className='w-[100px] h-2 bg-irisBlueColor rounded-full block mt-[-14px]'></span>
-              <p className='text__para text-blue'> Patient Satisfaction</p>
+              <span className='w-[100px] h-2 rounded-full block mt-[-14px]'></span>
+              <p className='text__para text-white'> Patient Satisfaction</p>
             </div>
 
           </div>
@@ -69,13 +71,15 @@ const Home = () => {
     </div>
   </section>
   {/*==========hero section end=====*/}
+
+
   <section>
     <div className="container">
       <div className='lg:w-[470px] mx-auto'>
         <h2 className='heading text-center'>
-          Providing the best medical services
+        Delivering  top-notch  healthcare services.
           </h2>
-        <p className='text__para text-center'>
+        <p className='text__para text-center text-[#2525A0]'>
           World-class care for everyone. Our healthsystem offers unmatched, 
           expert healthcare  
         </p>
@@ -88,10 +92,10 @@ const Home = () => {
             <img src= {icon01}  alt="icon1"/>
           </div>
           <div className="mt-[30px]">
-            <h2 className='text-[26px] leading-9 text-headingColor
+            <h2 className='text-[26px] leading-9 text-[#020279]
             font-[700] text-center'>Find a Doctor
             </h2>
-            <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>
+            <p className='text-[16px] leading-7 text-[#010143] font-[400] mt-4 text-center'>
               World-class care for everyone. 
               Our healthsystem offers unmatched, 
             expert healthcare. From the lab to the Clinic
@@ -111,12 +115,12 @@ const Home = () => {
           </div>
 
           <div className="mt-[30px]">
-            <h2 className='text-[26px] leading-9 text-headingColor
+            <h2 className='text-[26px] leading-9 text-[#020279]
             font-[700] text-center'>Find a Location
             </h2>
-            <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>
-              World-class care for everyone. Our healthsystem offers unmatched, 
-            expert healthcare. From the lab to the Clinic
+            <p className='text-[16px] leading-7 text-[#010143] font-[400] mt-4 text-center'>
+            Finding quality care just got easier.Find nearby clinics and hospitals, 
+            so you can receive expert care tailored to your needs, anytime, anywhere.
              </p>
              <Link to='/doctors' className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto
              flex items-center justify-center group hover:bg-primaryColor
@@ -133,12 +137,12 @@ const Home = () => {
           </div>
 
           <div className="mt-[30px]">
-            <h2 className='text-[26px] leading-9 text-headingColor
+            <h2 className='text-[26px] leading-9 text-[#020279]
             font-[700] text-center'>Book Appointment
             </h2>
-            <p className='text-[16px] leading-7 text-textColor font-[400] mt-4 text-center'>
-              World-class care for everyone. Our healthsystem offers unmatched, 
-            expert healthcare. From the lab to the Clinic
+            <p className='text-[16px] leading-7 text-[#010143] font-[400] mt-4 text-center'>
+            Choose your preferred doctor, select a convenient time, 
+            and secure your visit—experience seamless care tailored to your schedule.
              </p>
              <Link to='/doctors' className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto
              flex items-center justify-center group hover:bg-primaryColor
@@ -152,12 +156,13 @@ const Home = () => {
       </div>
     </div>
   </section>
+
   <About />
   {/*========services section=====*/}
   <section>
     <div className="container">
         <div className="xl:w-[470px] mx-auto">
-          <h2 className="heading text-center">Our Great Doctors</h2>
+          <h2 className="heading text-center">Services We Provide </h2>
           <p className="text__para text-center">
             World-class care for everyone. Our health system offers unmatched,expert health care.
           </p>
@@ -178,26 +183,26 @@ const Home = () => {
           </h2>
 
           <ul className="pl-4">
-            <li className="text__para">
+            <li className="text__para text-[#2525A0]">
               1.Schedule the Appointment directly.
             </li>
-            <li className="text__para">
+            <li className="text__para text-[#2525A0]">
               2.Search for your physician
             </li>
-            <li className="text__para">
-              3. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quam doloribus blanditiis laudantium dolores odit quasi neque maxime labore recusandae nemo eligendi architecto incidunt officiis distinctio aliquid, quos eaque inventore.
+            <li className="text__para text-[#2525A0]">
+              3. Consult for your Symptoms
             </li>
           </ul>
-          <Link to="/">
+          <Link to="/doctors">
           <button className="btn">Learn More</button>
           </Link>
         </div>
 
         {/*========features section images=====*/} 
-        <div className="relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0">
+        <div className="relative z-10 xl:w-[600px] flex justify-end mt-[50px] lg:mt-0">
           <img src={featureImg} className="w-3/4" alt="" />
         
-        <div className="w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 md:buttom-[100px] md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:px-4 lg:pb-[26px] rounded-[10px]">
+        <div className="w-[150px] lg:w-[248px] bg-white border border-[#D4DCF1] border-[2.5px]  absolute bottom-[50px] left-0 md:buttom-[100px] md:left-5 z-20 p-2 pb-3 lg:pt-4 lg:px-4 lg:pb-[26px] rounded-[10px]">
          
          
           <div className="flex items-center justify-between">
@@ -221,7 +226,7 @@ const Home = () => {
           <div className="flex items-center gap-[6px] lg:gap[10px] mt-2 lg:mt-[18px]">
             <img src={avatarIcon} alt="" />
             <h4 className="text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700] text-headingColor">
-              Wayne Collins
+              Alex Parker 
             </h4>
           </div>
           </div> 
@@ -232,10 +237,11 @@ const Home = () => {
   {/*========features section end=====*/}
   {/*========our great doc section=====*/}
   <section>
-  <div className="container">
+  <div className="container h-[1000px] bg-[#00004d]">
     <div className="xl:w-[470px] mx-auto">
-      <h2 className="heading text-center !text-blue-500">Our Great Doctors</h2>
-      <p className="text__para text-center">
+    <h2 className="heading text-center text-white mt-8">Our Great Doctors</h2>
+
+      <p className="text__para text-center text-white">
         World-class care for everyone. Our health system offers unmatched, expert health care.
       </p>
     </div>
